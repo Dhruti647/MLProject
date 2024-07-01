23f0e0828b3b05fab9212d6d8f68895743ed0db8
Dhruti647 <168404784+Dhruti647@users.noreply.github.com>
2024-07-01 15:31:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Dhruti',
+author_email='tdhruti5959@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
